7198c312dc121e4610df22e8bb07f6c05b6f0401
mohamadrezahedayati <hedayatimohamadreza@gmail.com>
2021-04-13 03:06:28 +0430

    finish chapter 2

---
+++ src/views/ItemList.vue
@@ -0,0 +1,14 @@
+<template>
+  <item />
+</template>
+
+<script>
+import Item from '../components/Item.vue'
+import api from '../api/api'
+
+export default {
+  components: {
+    Item
+  }
+}
+</script>
