55b857ac9ae72ea8c5b11f2b879f2c11c83e5ccc
mohamadrezahedayati <hedayatimohamadreza@gmail.com>
2021-04-13 13:52:24 +0430

    finish chapter 3

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class='hidden' :style="{'width': '0%'}">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
